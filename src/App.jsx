9f6252ef999c6492a7038ff871a6ed2e254c5d8b
Guilherme de Souza Silva <gui.iug.net@gmail.com>
2022-06-06 11:35:38 -0300

    Adding typescript config

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { Fragment } from "react/cjs/react.production.min";
-import { Counter } from "./components/Counter";
 import { RepositoryList } from "./components/RepositoryList";
 import "./styles/global.scss";
 
 export function App() {
   return (
-    <Fragment>
-      <RepositoryList />
-      <Counter />
-    </Fragment>
+    <RepositoryList />
   );
 }
